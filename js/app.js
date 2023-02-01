2090c09777c6d35e7ca56a313386f623ada6b762
KnightHONG0912 <sc22jh@leeds.ac.uk>
2023-02-01 15:13:01 +0000

    添加注册逻辑

---
+++ js/app.js
@@ -0,0 +1,16 @@
+window.app = {
+	
+	serverUrl : "http://127.0.0.1",
+	
+	isNotNull: function(str) {
+		if (str != null && str != "" && str != undefined) {
+			return true;
+		}
+		return false;
+	},
+	
+	validEmail: function(email) {
+		var regexp = /^[a-zA-Z0-9_-]+@[a-zA-Z0-9_-]+(\.[a-zA-Z0-9_-]+)+$/;
+		return regexp.test(email);
+	}
+}
