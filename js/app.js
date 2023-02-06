68c072d7e4119092b802ce6aa7a8b224337e3256
KnightHONG0912 <sc22jh@leeds.ac.uk>
2023-02-06 23:44:34 +0000

    注册，忘记密码，登录页面完善

--- js/app.js
+++ js/app.js
@@ -2,6 +2,10 @@ window.app = {
 	
 	serverUrl : "http://127.0.0.1:8068",
 	
+	USERINFO: "userInfo",
+	TIMEOUTVAL: 5000,//超时时间
+	WAITTIME: 4,
+	SALT: 'chqm-uftg*498.aa81@1',
 	isNotNull: function(str) {
 		if (str != null && str != "" && str != undefined) {
 			return true;
@@ -12,5 +16,15 @@ window.app = {
 	validEmail: function(email) {
 		var regexp = /^[a-zA-Z0-9_-]+@[a-zA-Z0-9_-]+(\.[a-zA-Z0-9_-]+)+$/;
 		return regexp.test(email);
+	},
+	
+	setGlobalCache: function(key, value){
+		var valueStr = JSON.stringify(value);
+		plus.storage.setItem(key",valueStr);
+	},
+	
+	getGlobalCache: function(key){
+		var value = plus.storage.getItem(key);
+		return JSON.parse(value);
 	}
 }
